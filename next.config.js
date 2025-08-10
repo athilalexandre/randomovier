22acb1bc4ed97bc72e358d12b8e05597d13d8671
Athila Alexandre <athilalexandre@gmail.com>
2025-08-10 11:55:20 -0300

    Update Next.js configuration to include custom Webpack settings for undici module transpilation and fallback for non-server environments. Added package-lock.json and updated package.json with new dependencies and devDependencies for Babel and Firebase. Adjusted TypeScript configuration to target ES2020.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['image.tmdb.org'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      }
+    }
+    
+    // Transpile undici module
+    config.module.rules.push({
+      test: /node_modules\/undici/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-transform-private-methods', '@babel/plugin-transform-class-properties']
+        }
+      }
+    })
+    
+    return config
   },
 }
 
